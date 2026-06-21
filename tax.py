2cb8c88d9232805b944480404a5736b77f61bda5
LSxwer <baltop14@gmail.com>
2026-06-21 14:14:29 +0700

    first commit

---
+++ tax.py
@@ -0,0 +1,25 @@
+import random
+import data
+hour = print(data.datasort())
+
+# def gross(hour):
+#     if hour <= 8 : 
+#         salary = hour * 15 
+#     elif hour > 8 : 
+#         salary = hour * 30 - 8 * 15
+#     return salary
+
+# def tax(gross_salary):
+#     if gross_salary < 300: 
+#         net = gross_salary * (1 - 0.12)
+#     elif 300 <= gross_salary < 600 :
+#         net = gross_salary * (1 - 0.15) 
+#     else : 
+#         net = gross_salary * (1 - 0.21)
+#     return net
+
+
+
+
+
+
